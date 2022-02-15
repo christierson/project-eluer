06d60e39e5742a12e006ceddd21177065a67a62c
Clemens Christierson <Christierson.c@gmail.com>
2022-02-15 19:30:32 +0100

    Update 31.py

--- 31.py
+++ 31.py
@@ -1,21 +1,61 @@
 coins = [200, 100, 50, 20, 10, 5, 2, 1]
+coins.reverse()
+coins_inv = [200/coin for coin in coins]
 
-def main():
-    backtrack([0]*8)
+    
+def get_sum(a):
+    return sum([x*y for (x, y) in zip(a, coins)])
 
-def backtrack(a, res):
+
+def backtrack(a, i, val):    
     sum = get_sum(a)
     if sum > 200:
-        return []
+        return backtrack(a[:i-1] + [a[i] + 1, 0] + a[i+1:], i-1, val)
     elif sum == 200:
-        return [a]
+        return backtrack(a[:i-1] + [a[i] + 1, 0] + a[i+1:], i-1, val + 1)
     else:
-        for i, coin in enumerate coins:
-
-
-
-def next(a, i):
+        c = a[i]
+        if c < coins_inv[i]:
+            backtrack(a[:i] + [a[i] + 1] + a[i+1:], i, val)
+        elif c == coins_inv[i]:
+            return val
+        
+        
+        
+               
+        if below_max(a, i):
+            backtrack(a, i+1, val)
+        else:
+            c = a[i]
+            a[:i] + [a[i] + 1] + a[i+1:]
+            if c < coins_inv[i]:
+                backtrack(a[:i] + [a[i] + 1] + a[i+1:], i+1, val)
+            elif c == coins_inv[i]:
+                pass
+    return res
     
+def next(a, i):
+    n = coins_inv[i]
+    if a[i == n]:
+        return a[:i+1] + [a[i+1] + 1] + a[-i:]
+    else:
+        return a[:i] + [a[i] + 1] + a[i+1:]
+
+
+
+def main():
+    print(backtrack([0]*8, 0))
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+def get_possible(n):
+    return list(filter(lambda c : c <= n, coins))
+    #return list(filter(lambda c : c <= n, [1, 2, 5, 10, 20, 50, 100, 200]))
 
 def backuptrack(n, c, d):
     if n == 0:
@@ -29,16 +69,3 @@ def backuptrack(n, c, d):
         print(d)
         return c
 
-def get_sum(a):
-    return sum([x*y for (x, y) in zip(a, coins)])
-
-
-
-def get_possible(n):
-    return list(filter(lambda c : c <= n, coins))
-    #return list(filter(lambda c : c <= n, [1, 2, 5, 10, 20, 50, 100, 200]))
-
-
-
-if __name__ == "__main__":
-    main()
